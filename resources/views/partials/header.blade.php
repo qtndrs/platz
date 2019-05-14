dadb7e4036baeb4659f386896f49ccd7ba1052c3
qtndrs <quentin.droussin@gmail.com>
2019-05-14 14:51:38 +0200

    Templating mis en place

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" type="image/png" href="img/small-logo-01.png">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,900italic,700italic,700,500italic,400italic,500,300italic,300' rel='stylesheet' type='text/css'>
 	<link  href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-	<link href='style.css' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
 
 
 
