bc51c9acb1cca1ae73f013cdb323e230593e3e71
qtndrs <quentin.droussin@gmail.com>
2019-06-12 16:01:50 +0200

    Ajax addComment ok
    Il reste à restreindre l'accès à la fonction aux utilisateurs connectés.

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -2,17 +2,20 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Models\Commentaire as CommentairesMdl;
+use App\User;
 
 
 
 class CommentaireController extends Controller{
 
   public function addComment(Request $request){
-       $commentaire = new CommentairesMdl;
-       $commentaire->texte = $request->input('body');
-       $commentaire->user_id = $request->input('user');
-       $commentaire->ressource_id = $request->input('ressource');
-       return ($commentaire->save())?1:0;
-   }
+      $commentaire = new CommentairesMdl;
+      $commentaire->texte = $request->input('body');
+      $commentaire->user_id = $request->input('user');
+      $commentaire->ressource_id = $request->input('ressource');
+      $commentaire->save();
+      $user = User::find($commentaire->user_id);
+      return $user;
 
+}
 }
--- public/js/add.js
+++ public/js/add.js
@@ -10,19 +10,16 @@
                       url: $(this).attr('action'),
                       data: $(this).serialize(),
                       method: 'get',
-                      success: function(reponsePHP){
-                        if (reponsePHP == 1) {
+                      success: function(user){
+
                           var code = "<div class=\"post-reply\">\n\
                               					<div class=\"image-reply-post\"></div>\n\
-                              				  <div class=\"name-reply-post\">"+$('#user').val()+"</div>\n\
+                              				  <div class=\"name-reply-post\">"+user['name']+"</div>\n\
                               					<div class=\"text-reply-post\">"+$('#body').val()+"</div>\n\
                                 			</div>";
                           $('#liste').prepend(code).find('.post-reply:first').hide().slideDown();
                           $('#body').val("");
-                        }
-                        else {
-                          alert("Problème durant l'ajout du commentaire");
-                        }
+
                       },
                       error: function(){
                         alert('Il y a une erreur de transaction');
